1e6fdfe2f4b813a5ade03b19239cababc2fd7e1a
BIGBEASTISHANK <pranjal3211p@gmail.com>
2021-02-02 16:06:56 +0530

    Deploy website - based on

--- 3e7575ca.85edb17a.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{75:function(a){a.exports=JSON.parse('{"allTagsPath":"/beta/blog/tags","slug":"first-blog","name":"first-blog","count":1,"permalink":"/beta/blog/tags/first-blog"}')}}]);
--- 90c9e26f.beea5b0c.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{80:function(a){a.exports=JSON.parse('{"allTagsPath":"/beta/blog/tags","slug":"about-me","name":"about-me","count":1,"permalink":"/beta/blog/tags/about-me"}')}}]);
